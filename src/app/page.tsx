d0915d462c99df6315bc3cca13ee18b43e6fbd00
Stuart MacCallum <maccalsa@googlemail.com>
2025-06-11 07:51:45 +0100

    03 better auth (#2)
    * Add better auth
    
    * Add better auth schema "generate" and copied
    
    * Add Better auth and a dirty client login implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,53 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch //refetch the session
+  } = authClient.useSession()
+
+  const onSubmit = async () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onSuccess: () => {
+        console.log("Success");
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <p>This is a test</p>
-      <div className="flex flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+    <div>
+      <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Sign In</Button>
+      {session?.user ? (
+        <div>
+          <p>Name: {session?.user?.name}</p>
+          <p>Email: {session?.user?.email}</p>
+        </div>
+      ) : (
+        <div>
+          <p>No session</p>
+        </div>
+      )}
     </div>
   );
 }
